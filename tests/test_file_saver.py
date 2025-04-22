c7a5addeffdabbe798c64b3b98b3195cc1c70962
MichalTesnar <michal.tesnar007@gmail.com>
2025-04-22 13:59:33 +0200

    dev:setting up pytest

--- tests/test_file_saver.py
+++
@@ -1,9 +0,0 @@
-from lib.file_saver import FileSaver
-
-random_json = {
-    "lol": "aaa"
-}
-
-fs = FileSaver()
-
-fs.save_file("12321", random_json)
--- tests/test_llm_agent.py
+++
@@ -1,7 +0,0 @@
-from lib.llm_agent import LLMAgent
-
-llm_agent = LLMAgent()
-
-response = llm_agent.respond("Tell me a quick joke.")
-
-print(response)
